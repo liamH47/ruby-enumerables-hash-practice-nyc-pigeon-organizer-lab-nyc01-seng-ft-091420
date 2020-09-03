6c8209185ce2775d10171a34f0be163710748302
friendly-machine-4891 <liamhanafee@gmail.com>
2020-09-03 19:42:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,14 +7,14 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
-      final_array[name] = {}
-end 
-    if !final_array[name][key]
-      !final_array[name][key] = []
-end
-final_array[name][key].push(inner_key)
-end
-end
-end
+          final_array[name] = {}
+        end 
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key)
+      end
+    end
+  end
   binding.pry 
 end
